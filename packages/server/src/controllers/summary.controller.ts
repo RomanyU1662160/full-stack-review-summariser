b8bed79cf6eabaac0737d66847820d95f2f79134
Romany <romanyfayiez@hotmail.com>
2026-01-01 18:30:15 +0000

    feat: handle edge cases

--- packages/server/src/controllers/summary.controller.ts
+++ packages/server/src/controllers/summary.controller.ts
@@ -4,6 +4,7 @@ import { AiChatService } from '../services/chat.service'
 import { logger } from '../utils'
 import type { Summary } from '../../generated/prisma/client'
 import { ProductsService } from '../services'
+import { ReviewsService } from '../services/reviews.service'
 
 export const SummaryController = {
   getReviewSummary: async (
@@ -13,24 +14,40 @@ export const SummaryController = {
     const { id } = req.params
     const productId = Number(id)
 
+    // Validate productId
     if (isNaN(productId)) {
       return res.status(400).json({ error: 'Invalid product ID' })
     }
 
+    // Check if product exists
     const existingProduct = await ProductsService.getProductById(productId)
     if (!existingProduct) {
       return res.status(404).json({ error: 'Product not found' })
     }
 
+    // Check for existing summary in the database
     const storedSummary =
       await SummariesService.getSummaryByProductId(productId)
     const now = new Date()
 
+    // If summary exists and is not expired, return it
     if (storedSummary && storedSummary.expiresAt > now) {
       logger.info(`Using cached summary for product ID: ${productId}`)
       return res.status(200).json({ summary: storedSummary })
     } else {
+      // If no summary exists or it has expired, generate a new one
+      // Check if there are reviews for the product, if none, return a message
+      const existingReview =
+        await ReviewsService.getReviewsByProductId(productId)
+      if (existingReview.length === 0) {
+        return res
+          .status(200)
+          .json({ message: 'No reviews found for this product' })
+      }
+
       logger.info(`Generating new summary for product ID: ${productId}`)
+
+      // Generate new summary using AI service
       const aiReviewSummary = await AiChatService.summarizeReviews(productId)
       logger.info(
         `Storing new summary in database for product ID: ${productId}`
